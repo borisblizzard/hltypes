c08295b16221da0f0a4644d8776aaf282b6ef2b1
Boris Mikic <boris.blizzard@gmail.com>
2021-02-18 13:31:01 +0100

    - added override keywords

--- lib/rapidxml/rapidxml.hpp
+++ lib/rapidxml/rapidxml.hpp
@@ -83,7 +83,7 @@ namespace rapidxml
 
         //! Gets human readable description of error.
         //! \return Pointer to null terminated description of the error.
-        virtual const char *what() const override throw()
+        virtual const char *what() const throw()
         {
             return m_what;
         }
